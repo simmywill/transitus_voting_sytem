8952738319a114c7ab53ee732b8d6f21f23fd9a9
simmywill <simeoncwilliams@gmail.com>
2024-12-03 11:45:34 -0400

    voting results #9

--- voting_system/urls.py
+++ voting_system/urls.py
@@ -44,7 +44,8 @@ urlpatterns = [
     path('voter_session/verify/<uuid:session_uuid>/', voters_views.voter_verification, name = 'voter_verification'),
     path('voter_session/<uuid:session_uuid>/', voters_views.voter_session, name = 'voter_session'),
     path('submit-vote/<uuid:session_uuid>/', voters_views.submit_vote, name='submit_vote'),
-    path('tally-votes/<uuid:session_uuid>/', voters_views.tally_votes, name='tally_votes'),
+    path('tally-votes/<uuid:session_uuid>/', voters_views.tally_votes, name = 'tally_votes'),
+    path('segment_results/<uuid:session_uuid>/', voters_views.results , name = 'results'),
 
 
 
