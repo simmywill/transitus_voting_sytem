bd2f053ab09c6bb9240e7cdae6cc24fe5486ad4f
simmywill <simeoncwilliams@gmail.com>
2025-11-09 16:15:09 -0400

    storage fixes #1

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -92,8 +92,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'storages'
+    'django.contrib.staticfiles'
     
 ]
 
